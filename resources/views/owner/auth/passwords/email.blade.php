8afd42c95f3ed4e2a97191c213d6af2e9967465f
Tushar Kanta Nayak <tusharkanta@rixosys.com>
2022-11-01 13:09:51 +0530

    kuafor owner panel design

--- resources/views/owner/auth/passwords/email.blade.php
+++ resources/views/owner/auth/passwords/email.blade.php
@@ -1,47 +1,75 @@
-@extends('owner.layout.auth')
+<!DOCTYPE html>
+<html lang="en">
 
-<!-- Main Content -->
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Kuafor</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('owner/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('owner/assets/vendors/css/vendor.bundle.base.css')}}">
+
+    <link rel="stylesheet" href="{{asset('owner/assets/css/style.css')}}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{asset('owner/assets/images/favicon.ico')}}" />
+</head>
+
+<body>
+    <div class="container-scroller">
+        <div class="col-md-4 offset-md-4 mt-5 pt-5">
+            <div class="card1">
+                <div class="card-body">
+                    <h3 class="page-title">Forgot Your Password?</h3>
+                    <p class="card-description">Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                        amet, consectetur, adipisci velit.. </p>
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/owner/password/email') }}">
+                    <form class="forms-sample" role="form" method="POST"
+                        action="{{ url('/owner/password/email') }}">
                         {{ csrf_field() }}
-
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
+                        <button type="submit" class="btn btn-primary me-2 w-100">Reset Password</button>
+                        <br><br>
+                        <div class="text-center">
+                            <span>Know your password?<a href="{{ url('/owner/login') }}" style="color: #49654e">Login</a></span>
                         </div>
                     </form>
                 </div>
             </div>
+            <!-- page-body-wrapper ends -->
         </div>
     </div>
-</div>
-@endsection
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="{{asset('owner/assets/vendors/js/vendor.bundle.base.js')}}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="{{asset('owner/assets/vendors/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('owner/assets/js/jquery.cookie.js')}}" type="text/javascript"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{asset('owner/assets/js/off-canvas.js')}}"></script>
+    <script src="{{asset('owner/assets/js/hoverable-collapse.js')}}"></script>
+    <script src="{{asset('owner/assets/js/misc.js')}}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="{{asset('owner/assets/js/dashboard.js')}}"></script>
+    <script src="{{asset('owner/assets/js/todolist.js')}}"></script>
+    <!-- End custom js for this page -->
+</body>
+
+</html>
